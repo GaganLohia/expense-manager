c1f2380329726f7e6ef2fe6268ab17b0b3835854
Gagan Lohia <gagans652@gmail.com>
2018-09-22 23:39:59 +0530

    Signup Started

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-//Required Libraries
-var express     = require('express'),
-    mongoose    = require('mongoose');
+var app     = require('express')(),
+    mongoose    = require('mongoose'),
+    path        = require('path'),
+    config      = require(path.resolve( __dirname, "./config.js" )),
+    user        = require(path.resolve(__dirname,"./models/user.js")),
+    bodyParser  = require('body-parser'),
+    port        = process.env.PORT || 3000;
 
-//Setting up
-var app = express();
-mongoose.connect('mongodb://gaganlohia:Password1@ds237072.mlab.com:37072/expense-manager')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+mongoose.connect(config.dbUrl);
 var db = mongoose.connection;
 
-//App Code
+
 db.once('open',function(){
     console.log('Database Connected');
 });
-db.on('error',console.error.bind(console, 'connection error:'));
+db.on('error',console.error.bind(
+    console, 'connection error:'
+));
+
+
 app.get('/', function(req, res){
-    res.send('hello world');
-  });
-// just checking the commit
-//////////////////////////////////////////////////
-app.listen(3000);
+    res.send('This is Api End-point for Expense Manager');
+});
+app.post('/signup',function(req,res){
+     var userName = req.body.username;
+     var password = req.body.password;
+     res.send(userName+" "+password);
+     User.findOne({
+        username : userName
+     },function(err,user){
+
+     })
+});
+app.listen(port,()=>{
+    console.log('Listening to Port ' + port);
+});

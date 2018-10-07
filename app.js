68efe08f6035a1001db6e9c51fa84646fc02789a
Harshit Agrawal <harshit.harshitag@gmail.com>
2018-10-07 14:20:46 +0530

    Wild Path Added

--- app.js
+++ app.js
@@ -5,21 +5,25 @@ var app             = require('express')(),
     Authenticate    = require(path.resolve(__dirname,"./modules/authenticate.js")),
     Transactions    = require(path.resolve(__dirname,"./modules/transaction.js")),
     InitDb          = require(path.resolve(__dirname,"./modules/initDb.js")),
-    utils           = require(path.resolve( __dirname, "./modules/utilities" )),
+    utils           = require(path.resolve(__dirname, "./modules/utilities" )),
     port            = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-app.get('/', ()=>{
-    res.send('This is Api End-point for Expense Manager');
-});
+
 app.post('/signup',Authenticate.signUp);
 app.post('/signin',Authenticate.signIn);
 app.post('/addNewTransaction',utils.isTokenValid,Transactions.addNewTransaction);
 app.get('/getAllTransactions',utils.isTokenValid,Transactions.getAllTransactions);
 app.post('/updateTransaction',utils.isTokenValid,Transactions.updateTransaction);
+app.get('/', ()=>{
+    res.send('This is Api End-point for Expense Manager');
+});
+app.use('/*',(req,res)=>{
+    utils.sendResponse(res, 400, false, 'Bad Request!');
+})
 app.listen(port,()=>{
     console.log('Listening to Port ' + port);
     InitDb.init();
